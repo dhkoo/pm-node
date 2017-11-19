012c036cc8400280883c2d950d3a39321a6068a6
dhkoo <amainlog@gmail.com>
2017-11-19 22:59:29 +0900

    add auto linking beeond in recomplie...sh

--- beeond/recompile_beegfs_storage_stream.sh
+++ beeond/recompile_beegfs_storage_stream.sh
@@ -7,13 +7,16 @@ source_path="/home/kau/beegfs_stream/beegfs_storage/build"
 make -j16 -C $source_path
 
 if [ -e ${bin_path}/${inst} ]; then
-  echo "compile done"
+  echo "[Compile done]"
 else
+  echo "maybe It's first time"
+  sudo mv ${bin_path}/beeond ${bin_path}/origin_beeond
+  sudo ln -s /home/kau/beegfs_stream/beeond/source/beeond ${bin_path}/beeond
   echo "$(ls $bin_path | grep $inst)"
-	echo "[INFO] original ${inst} file exist"
 	sudo ln -s ${source_path}/beegfs-storage ${bin_path}/${inst}
 
-	echo "complie done"
-	echo "making symbolic link to beegfs-storage in beegfs_stream source"
+	echo "[complie done]"
+	echo "[beeond -> origin_beeond and symbolic link beeond modified by dhkoo]"
+	echo "[making symbolic link to beegfs-storage in beegfs_stream source]"
 fi
 
